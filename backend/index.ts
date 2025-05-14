67e1552685446a0b6964f50205d5b2e8107e491c
Lukasronnholm <bengtssonronnholm@gmail.com>
2025-05-14 15:58:00 +0200

    påbrjat

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,17 @@
-import cors from 'cors'
-import express from 'express'
-import * as sqlite from 'sqlite'
-import { Database } from 'sqlite'
-import sqlite3 from 'sqlite3'
+import { error } from "console";
+import cors from "cors";
+import express, { response } from "express";
+import { request } from "https";
+import * as sqlite from "sqlite";
+import { Database } from "sqlite";
+import sqlite3 from "sqlite3";
 
 let database: Database
 
 ;(async () => {
   database = await sqlite.open({
     driver: sqlite3.Database,
-    filename: 'test.sqlite'
+    filename: 'data.sqlite'
   })
 
   await database.run('PRAGMA foreign_keys = ON')
@@ -21,10 +23,16 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (_request, response) => {
-  response.send('Hello World!')
-})
+app.get("/", async (request, response) => {
+    try { 
+        const teams = await database.all("SELECT * FROM teams") 
+        response.json (teams)
 
-app.listen(3000, () => {
-  console.log('Webbtjänsten kan nu ta emot anrop.')
+    }catch(error){
+        console.error(error)
+        response.status(500).json({error:'Lag hittades ej'})
+    
+}})
+app.listen(5000, ()=>{
+    console.log('http://localhost:5000/')
 })
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState} from "react"
+
+interface Teams {
+    id: number
+    name: string
+    position: number
+    played: number
+    wins: number
+    draws: number
+    losses: number
+    goal_difference: number
+    points: number
+}
+
+function Home(){
+     
+    const [teams, setTeams] = useState<Teams[]>([])
+useEffect(()=>{
+    fetch ('http://localhost:5000')
+    .then((response)=> response.json())
+    .then((result)=>{
+        setTeams(result)
+    })
+
+},[])
+return(
+       
+        <>
+        
+             <ul className="ContainerText">{teams.map((team)=> (
+                <li key={team.id}>{team.position} {team.name} {team.played} {team.wins} {team.draws} {team.losses} {team.goal_difference} {team.points}</li>))}</ul>
+       </>
+       
+       )
+        
+    
+}
+export default Home
