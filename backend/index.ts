95958f9cff768d20b8e6b030caefc5b5161581bb
Lukasronnholm <bengtssonronnholm@gmail.com>
2025-06-08 18:28:10 +0200

    typat backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from 'express'
+import express,{Request, Response} from 'express'
 import * as sqlite from "sqlite";
 import { Database } from "sqlite";
 import sqlite3 from "sqlite3";
@@ -22,10 +22,10 @@ app.use(express.json())
 })()
 
 
-app.get("/", async (_request, response) => {
+app.get("/", async (_request:Request, response:Response) => {
     try { 
         const teams = await database.all("SELECT * FROM teams") 
-        response.json (teams)
+        response.status(200).json(teams)
 
     }catch(error){
         console.error(error)
@@ -33,10 +33,10 @@ app.get("/", async (_request, response) => {
     
 }})
 
-app.get("/players", async (_request, response) => {
+app.get("/players", async (_request:Request, response:Response) => {
     try { 
         const players = await database.all("SELECT * FROM players") 
-        response.json (players)
+        response.status(200).json(players)
 
     }catch(error){
         console.error(error)
@@ -46,7 +46,7 @@ app.get("/players", async (_request, response) => {
 
 
 
-app.post('/my-team', async ( request, response) =>{
+app.post('/my-team', async ( request:Request, response:Response) =>{
     const {player_id} = request.body
     try {
         if(!player_id){
@@ -63,7 +63,7 @@ app.post('/my-team', async ( request, response) =>{
         
     }
 });
-app.get("/my-team", async (_request, response) => {
+app.get("/my-team", async (_request:Request, response:Response) => {
     try { 
         const players = await database.all(
             `SELECT players.* 
@@ -71,7 +71,7 @@ app.get("/my-team", async (_request, response) => {
             JOIN players ON players.id = created_team_players.player_id 
             WHERE created_team_players.team_id = 1 
             LIMIT 11`) 
-        response.json (players)
+        response.status(200).json (players)
 
     }catch(error){
         console.error(error)
@@ -79,7 +79,7 @@ app.get("/my-team", async (_request, response) => {
     
 }})
 
-app.delete("/my-team", async (_request, response) => {
+app.delete("/my-team", async (_request:Request, response:Response) => {
     try { 
        await database.run('DELETE FROM created_team_players WHERE team_id = 1') 
         response.status(200).json({message:'Laget har tagits bort'})
@@ -89,7 +89,7 @@ app.delete("/my-team", async (_request, response) => {
         response.status(500).json({error:'Laget kunde inte tas bort'})
     
 }})
-app.get('/:id', async (request, response)=>{
+app.get('/:id', async (request:Request, response:Response)=>{
     try {
     const playerInfo = await database.all('SELECT * FROM players WHERE team_id=?',[
         request.params.id ])
